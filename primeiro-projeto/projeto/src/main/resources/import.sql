29644530820c0d261e5f36b60771afef7ba9e757
Thais Zimmer <165618062+thaisczimmer@users.noreply.github.com>
2025-09-05 01:21:50 +0000

    aulasergio04/09projeto2

--- primeiro-projeto/projeto/src/main/resources/import.sql
+++ primeiro-projeto/projeto/src/main/resources/import.sql
@@ -1,3 +1,23 @@
 insert into Categoria (nome) values ('Bebidas');
 insert into Categoria (nome) values ('Frutas');
-insert into categoria (nome) values ('Carnes');
+insert into Categoria (nome) values ('Carnes');
+insert into Categoria (nome) values ('Higiene');
+insert into Categoria (nome) values ('Limpeza');
+insert into Categoria (nome) values ('Eletronicos');
+insert into Categoria (nome) values ('Roupas');
+insert into Categoria (nome) values ('Moveis');
+insert into Categoria (nome) values ('Livros');
+insert into Categoria (nome) values ('Brinquedos');
+
+insert into Fabricante (nome) values
+insert into Fabricante (nome) values
+insert into Fabricante (nome) values
+insert into Fabricante (nome) values
+insert into Fabricante (nome) values
+insert into Fabricante (nome) values
+insert into Fabricante (nome) values
+insert into Fabricante (nome) values
+insert into Fabricante (nome) values
+insert into Fabricante (nome) values
+
+insert into Produtos
